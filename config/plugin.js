cd2a663d1d69823070bc00b58f5957eab61e243f
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-01-16 17:49:50 +0800

    fix: solve cors by changing plugin order

--- config/plugin.js
+++ config/plugin.js
@@ -3,12 +3,6 @@
 // had enabled by egg
 // exports.static = true;
 
-
-exports.cors = {
-  enable: true,
-  package: 'egg-cors',
-};
-
 exports.sequelize = {
   enable: true,
   package: 'egg-sequelize',
@@ -27,4 +21,9 @@ exports.io = {
 exports.jwt = {
   enable: true,
   package: 'egg-jwt',
+};
+
+exports.cors = {
+  enable: true,
+  package: 'egg-cors',
 };
