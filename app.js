38e59f68de45a0e5b72e98409cd203b9646ff732
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-01-29 17:12:54 +0800

    feat: create default user

--- app.js
+++ app.js
@@ -2,8 +2,19 @@
 
 module.exports = app => {
   app.beforeStart(async () => {
-    await app.model.sync({
-      force: false,
-    });
+    const defaultAdmin = {
+      username: 'suyi',
+      password: '123456',
+      userrole: 'admin',
+    };
+
+    const existAdmin = await app.model.Member.findOne(defaultAdmin);
+    app.logger.info('#existAdmin:\n', existAdmin);
+
+    if (existAdmin) return;
+
+    const newAdmin = new app.model.Member(defaultAdmin);
+    await newAdmin.save();
+    app.logger.info('#newAdmin:\n', newAdmin);
   });
 };
