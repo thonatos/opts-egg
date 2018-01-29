fa0a80d18945bf9662f7ade01e37b7be2715e6c7
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-01-29 17:11:16 +0800

    feat: hook auth

--- app/middleware/dockerHookAuth.js
+++ app/middleware/dockerHookAuth.js
@@ -3,7 +3,9 @@
 module.exports = () => {
   return async function(ctx, next) {
     const accessToken = ctx.get('X-AccessToken') || ctx.query.accessToken || null;
-    const hook = await ctx.service.hook.find(accessToken);
+    const hook = await ctx.model.Hook.findOne({
+      accessToken,
+    });
     if (!hook) {
       ctx.throw(403, '#hook: auth failed');
       return;
