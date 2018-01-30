8e85859972e70b2e408b74cca63f721719c1ce72
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-01-30 11:00:19 +0800

    feat: enhance member model

--- app/model/member.js
+++ app/model/member.js
@@ -1,17 +1,33 @@
 'use strict';
 
+const crypto = require('crypto');
+
 module.exports = app => {
   const mongoose = app.mongoose;
   const Schema = new mongoose.Schema({
-    username: String,
-    password: String,
+    username: {
+      type: String,
+      unique: true,
+    },
     userrole: String,
+    salt: String,
+    hash: String,
   }, {
     timestamps: {
       createdAt: 'created_at',
       updatedAt: 'updated_at',
     },
   });
-
+  Schema.methods.setPassword = function(password) {
+    const salt = crypto.randomBytes(16).toString('hex');
+    const hash = crypto.createHmac('sha256', salt).update(password).digest('hex');
+    this.salt = salt;
+    this.hash = hash;
+  };
+  Schema.methods.validPassword = function(password) {
+    const salt = this.salt;
+    const hash = crypto.createHmac('sha256', salt).update(password).digest('hex');
+    return this.hash === hash;
+  };
   return mongoose.model('Member', Schema);
 };
