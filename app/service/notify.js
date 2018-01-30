a6cac32ba80edbea7b0d3d5df44bb8a837578765
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-01-30 15:09:18 +0800

    refactor: export send method

--- app/service/notify.js
+++ app/service/notify.js
@@ -4,14 +4,39 @@ const axios = require('axios');
 const Service = require('egg').Service;
 
 class NotifyService extends Service {
+  async send(url, msg, opts = {}) {
+    const { ctx } = this;
+    switch (opts.type) {
+      case 'dingtalk':
+        return this.sendToDingtalkRobot(...arguments);
+
+      default:
+        ctx.logger.info('not implemented');
+        break;
+    }
+    return null;
+  }
+
   // https://open-doc.dingtalk.com/docs/doc.htm?spm=a219a.7629140.0.0.karFPe&treeId=257&articleId=105735&docType=1
-  async sendToDingtalkRobot(callbackUrl, message) {
-    if (!callbackUrl) return;
+  async sendToDingtalkRobot(url, data) {
+    const { ctx } = this;
+    const { image, tag } = data;
+    const msg = {
+      msgtype: 'markdown',
+      markdown: {
+        title: '#Image Pushed',
+        text: `${image.name} \n\n` +
+          `> 区域：${image.region} \n\n` +
+          `> 版本：${tag.tag} \n\n` +
+          `> 时间：${tag.pushed_at} \n\n`,
+      },
+    };
+    if (!url) return;
     try {
-      const { data } = await axios.post(callbackUrl, message);
-      console.log('#nofity.sendToDingtalkRobot', data);
+      const { data } = await axios.post(url, msg);
+      ctx.logger.info('#nofity.sendToDingtalkRobot', data);
     } catch (error) {
-      console.log(error);
+      ctx.logger.error(error);
     }
   }
 }
