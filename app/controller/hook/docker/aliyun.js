05d2c566bc0459af5532812c0eee062fdd0a0a6c
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-02-03 17:18:25 +0800

    refactor: remove old trigger

--- app/controller/hook/docker/aliyun.js
+++ app/controller/hook/docker/aliyun.js
@@ -5,23 +5,16 @@ const Controller = require('egg').Controller;
 class AliyunController extends Controller {
   async create() {
     const { ctx } = this;
-    const { deployId } = ctx.query;
     const { callbackUrl } = ctx.hook || {};
     const body = ctx.request.body;
 
     // 记录镜像
     const data = await this.ctx.service.image.create(body);
 
-    // 更新应用
-    if (deployId) {
-      await ctx.service.deploy.update(deployId);
-    }
-
     // 发送通知
     await ctx.service.notify.send(callbackUrl, data, {
       type: 'dingtalk',
     });
-
     ctx.status = 204;
   }
 }
