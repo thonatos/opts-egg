87e7974721b814ca6f06281a7074909be9fc7e2c
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2018-02-09 09:41:32 +0800

    feat: change welcome info

--- app/controller/home.js
+++ app/controller/home.js
@@ -9,7 +9,12 @@ const Controller = require('egg').Controller;
  */
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'console.egg';
+    this.ctx.body = `
+      <div style="margin: 0 auto; padding: 1em 0; max-width: 960px;">
+        <h2>(Dev)Opts</h2>
+        <p style="font-size: 14px;">Powered By Egg.js</p>
+      </div>
+    `;
   }
 }
 
